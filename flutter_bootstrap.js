2559a77e6e4eacaf3bd1171c13d43a1984788e28
Sergio <sergioorigua@gmail.com>
2024-08-04 01:18:45 -0500

    add Guia del Cacao

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"d89dd85f25cf4a3e15465a59594f4125b7f48d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3982210565"
+    serviceWorkerVersion: "3256974296"
   }
 });
